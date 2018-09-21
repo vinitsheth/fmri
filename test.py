376f011e364499311f37691d6506736a54a71eee
aadhav1395 <aadhavan@asu.edu>
2018-09-20 17:01:38 -0700

    made changes to path to data

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from scipy.io import loadmat
 import DataManupulation
-mat = loadmat('/Users/vinitsheth/SmlFinalProject/Data/data-starplus-05710-v7.mat')
+mat = loadmat('Data/data-starplus-05710-v7.mat')
 
 mata = DataManupulation.getMata(mat)
-print mata
+print (mata)
