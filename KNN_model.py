c5aad511168ce6cb77a3c374b8f4074220f7009b
Manjusha <mravind1@asu.edu>
2018-10-03 08:59:49 -0700

    KNN Model Skeleton

---
+++ KNN_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 08:39:15 2018
+
+@author: manjusharavindranath
+
+
+"""
+
+from load_dataset import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+
+def trainKNN(dataTrain, labTrain, dataTest, labTest):
+    
+    
+def predictKNN(kLabels,k):
