d94e74df6dc81663b15fafe06d17e3656517ec91
Manjusha <mravind1@asu.edu>
2018-11-19 08:50:46 -0700

    ROC changes

--- KNN_model.py
+++ KNN_model.py
@@ -27,3 +27,31 @@ def predictKNN(kLabels,k):
         curLabel=values[ind]
         retPrediction[i,0]=curLabel
     return retPrediction
+
+def predictKNNConfBinary(kLabels,k): #Binary means classes are just 0 or 1
+    retPrediction=np.zeros((np.shape(kLabels)[0],1))
+    for i in range(0,np.shape(kLabels)[0]):
+        neighborLabelsCurTest=kLabels[i,:][0:k]
+        retPrediction[i,0]=np.sum(neighborLabelsCurTest)/len(neighborLabelsCurTest)
+    return retPrediction
+
+
+
+def computeROC(confVals, labels):
+    #Sweep through thresholds
+    thresholds=list(np.unique(confVals))
+    #Need a -1 threshold
+    thresholds=[-1]+thresholds
+    pds=[]
+    pfas=[]
+    for curThresh in thresholds:
+        #If the confidence is above threshold assign class 1, otherwise assign class 0
+        curLabelsThresh=confVals>curThresh
+        #Compute pd and pf
+        indexPos=labels==1
+        indexNeg=labels==0
+        curPd=np.sum(curLabelsThresh[indexPos])/np.sum(labels) #Sum of true positives divided by number of positives
+        curPfa=np.sum(curLabelsThresh[indexNeg])/np.sum(labels==0) #Sum of false positives divided by number of negatives
+        pds.append(curPd)
+        pfas.append(curPfa)
+    return pds,pfas  
