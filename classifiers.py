74c13808f64f23cb8ca5b20e402cc5d06067724a
aadhav1395 <aadhavan@asu.edu>
2018-11-29 18:29:49 -0700

    classifiers.py

--- classifiers.py
+++ classifiers.py
@@ -4,34 +4,115 @@ Created on Fri Oct 19 14:07:01 2018
 
 @author: 
 """
-import argparse
-from utils.constants import classifiers, PathToSubjects
-from operator import itemgetter 
-from collections import Counter
-from load_data import get_subject_data
 
+import argparse
+import pandas as pd
+import os
+
+from data_extract import extractData
+from utils.constants import classifiers, PathToSubjects
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, roc_auc_score,accuracy_score
+from sklearn.model_selection import StratifiedKFold
+
+#from GNB import GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+def PrepareData(meta, info, data):
+    
+    Picture = []
+    Sentence = []
+    
+    for i in range(meta['ntrials']):
+        TrialInfo = info[i]
+        TrialData = data[i]
+        cond = TrialInfo['cond']
+        stimulus = TrialInfo['firstStimulus']
+        if cond == 2 or cond == 3:
+            pic, sen = [],[]
+            if stimulus == 'P':
+                pic = TrialData[:16]
+                sen = TrialData[16:32]
+            else:
+                pic = TrialData[16:32]
+                sen = TrialData[:16]
+            
+            Picture.append(sum(pic, []))
+            Sentence.append(sum(sen, []))
+            #Picture.append(pic)
+            #Sentence.append(sen)
+  
+    SentenceFrame = pd.DataFrame.from_records(Sentence)
+    PictureFrame = pd.DataFrame.from_records(Picture)
+    SentenceFrame['label'] = 0
+    PictureFrame['label'] = 1
+    data = pd.concat([SentenceFrame, PictureFrame], axis = 0)
+    return data
+
+def stratified(data, fold):
+    
+    skf = StratifiedKFold(n_splits=fold, random_state = 42)
+    X = data.iloc[:,:-1].values
+    y = data.iloc[:,-1].values.tolist()
+    i = 0
+    accuracy = []
+    for train_index, test_index in skf.split(X, y):
+        
+        X_train, X_test = data.iloc[train_index,:-1].values, data.iloc[test_index,:-1].values
+        y_train, y_test = data.iloc[train_index,-1].values.tolist(), data.iloc[test_index,-1].values.tolist()
+    
+        # Feature Scaling
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
+        
+        classifier = GaussianNB()
+        
+        classifier.fit(X_train, y_train)
+
+        score = classifier.score(X_test, y_test)
+        
+        accuracy.append(score)
+        i+=1
+    print(sum(accuracy)/len(accuracy))
+    return sum(accuracy)/len(accuracy)   
+    
 if __name__ == "__main__":
     
     # Ignore matplotlib and numpy warnings
     import warnings
     warnings.filterwarnings("ignore")
     
-
+    #
+    #input algorithm for classification
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-model', type =int, help='Classification Algorithm. 1-GNB, 2-LogReg, 3-KNN, 4-SVM')          
-    parser.add_argument('-subject', metavar='S', type=int, nargs='+',
-                    help='subjects to be considered for classification. ex.,-subject 1 2 3 ')
-    parser.add_argument('-sheepda', type=float,
-                    help='Learning Rate for the classifier', default = 0.1)
+    parser.add_argument('-model', type =int, help='Classification Algorithm. 1-GNB, 2-LogReg, 3-KNN, 4-SVM, 5- Random Forest')          
     args = vars(parser.parse_args())
     
     model = args['model']
-    subject = args['subject']
-    subject = [item-1 for item in subject]
-    sheepda = args['sheepda']
-    if len(subject) > 1:
-        subjectPath = list(itemgetter(*subject)(PathToSubjects))
-    else:
-        subjectPath = [PathToSubjects[subject[0]]]
-    LoadedData = get_subject_data(subjectPath)
-    print(Counter(LoadedData[1]))
+    print(classifiers[model-1])
+    
+    MatFilesPath = []
+    
+    for files in os.listdir(PathToSubjects):
+        MatFilesPath.append(os.path.join(PathToSubjects, files))
+    
+    acc = []
+    #
+    #Load data for each subject
+    for i in range(len(MatFilesPath)):
+        print("Loading data from matfile for subject {}".format(i+1))
+        file = MatFilesPath[i]
+        meta, info, data = extractData(file)
+        print("Preparing data for classification on subject {}".format(i+1))
+        
+        print("Classification on subject {}".format(i+1))
+        ClassificationData = PrepareData(meta, info, data)
+        accuracy = []
+        for k in range(2,42,2):
+            
+            accuracy.append(stratified(ClassificationData, k))
+            print("Fold {} : {}".format(k, accuracy[-1]))
+            
+        acc.append(max(accuracy))
--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-classifiers = ['GNB', 'LogReg', 'KNN', 'SVM']
+classifiers = ['GNB', 'LogReg', 'KNN', 'SVM', 'RF']
 
-PathToSubjects = ["Data/ExtractedData/Subject_04799/",
-            "Data/ExtractedData/Subject_04820",
-            "Data/ExtractedData/Subject_04847",
-            "Data/ExtractedData/Subject_05675",
-            "Data/ExtractedData/Subject_05680",
-            "Data/ExtractedData/Subject-05710"]
+PathToSubjects = "Data/subject"
